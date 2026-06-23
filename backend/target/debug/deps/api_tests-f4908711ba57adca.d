71dc78ce25d64f68e3e86c8ad08426ede483b7d3
Micheal Samuel <mykelsown@gmail.com>
2026-06-23 20:49:25 +0000

    tested: project passes strict linting tests

---
+++ backend/target/debug/deps/api_tests-f4908711ba57adca.d
@@ -0,0 +1,9 @@
+/workspaces/InheritX/backend/target/debug/deps/api_tests-f4908711ba57adca.d: tests/api_tests.rs Cargo.toml
+
+/workspaces/InheritX/backend/target/debug/deps/libapi_tests-f4908711ba57adca.rmeta: tests/api_tests.rs Cargo.toml
+
+tests/api_tests.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
--- backend/target/debug/deps/inheritx_backend-c4f6120f6c6c5a73.d
+++ backend/target/debug/deps/inheritx_backend-c4f6120f6c6c5a73.d
@@ -11,5 +11,5 @@ src/telemetry.rs:
 src/yield_calculator.rs:
 Cargo.toml:
 
-# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
 # env-dep:CLIPPY_CONF_DIR
